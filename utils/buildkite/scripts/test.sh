e7c8e136437014c58643bd87a19adce97d444d13
Ryan Peng <ypangus@gmail.com>
2023-05-21 16:53:31 -0700

    update test script with failed message

--- utils/buildkite/scripts/test.sh
+++ utils/buildkite/scripts/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-PROFILE="config-profile/software-update-setting.mobileconfig"
+PROFILE="config-profile/software-update-setting1.mobileconfig"
 if [ -f "$PROFILE" ]; then
     echo "Test pass!"
+    exit 0
 else
+    echo "Test failed!"
     exit 1
 fi
