9c2d36845526108ec85c3eb80b202cafee6a1a33
Ryan Peng <ypangus@gmail.com>
2023-05-22 15:25:48 -0700

    update plist_check script and software-update-setting profile for testing

--- utils/buildkite/scripts/plist_check.py
+++ utils/buildkite/scripts/plist_check.py
@@ -1,12 +1,29 @@
 import plistlib
+import subprocess
 
 def check_plist_keys():
-    file = "/Users/rypus/Code/github/repos/rypus/buildkite-testing/config-profile/software-update-setting.mobileconfig"
-    with open(file, 'rb') as infile:
-        plist = plistlib.load(infile)
+    # Get profile changes in this PR
+    command = [ "git", "diff", "--name-only", "main"]
+    res = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='UTF-8')
+    files = res.stdout.read().strip().split()
 
-    required_keys = [ 'PayloadIdentifier', 'PayloadDisplayName', 'PayloadDescription' ]
-    assert all(key in plist.keys() for key in required_keys)
+    if len(files) > 0:
+        profiles_check_result = {'valid': [], 'invalid': []}
+        for file in files:
+            if file.startswith('config-profile/unsigned'):
+                file_path = './' + file
+                with open(file_path, 'rb') as infile:
+                    plist = plistlib.load(infile)
+                    required_keys = [ 'PayloadIdentifier', 'PayloadDisplayName', 'PayloadDescription' ]
+                    res = all(key in plist.keys() for key in required_keys)
+                    if res:
+                        profiles_check_result['valid'].append(file.split('/')[-1])
+                    else:
+                        profiles_check_result['invalid'].append(file.split('/')[-1])
+    if len(profiles_check_result['invalid']) > 0:
+        print(f"Invalid profile found: {','.join(profiles_check_result['invalid'])}")
+        assert len(profiles_check_result['invalid']) == 0
+
 
 if __name__ == "__main__": 
     check_plist_keys()
